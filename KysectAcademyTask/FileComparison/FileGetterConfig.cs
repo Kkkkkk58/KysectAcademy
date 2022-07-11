9ced91cf465a90678391185b97031d9ed6628528
Karim Khasan <karimhasan3879@gmail.com>
2022-07-11 21:21:07 +0300

    refactor: add ctors to structs

--- KysectAcademyTask/FileComparison/FileGetterConfig.cs
+++ KysectAcademyTask/FileComparison/FileGetterConfig.cs
@@ -4,4 +4,10 @@ internal struct FileGetterConfig
 {
     public string FolderName { get; init; }
     public FileOptions FileOptions { get; init; }
+
+    public FileGetterConfig(string folderName, FileOptions fileOptions)
+    {
+        FolderName = folderName;
+        FileOptions = fileOptions;
+    }
 }
