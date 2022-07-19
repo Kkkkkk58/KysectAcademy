14ef4d90ee97be5a0dc04510521893cc6619b4f7
Karim Khasan <karimhasan3879@gmail.com>
2022-07-19 22:45:33 +0300

    refactor: make it more flexible by implementing configuration struct

--- KysectAcademyTask/Utils/ProgressTracking/ConsoleComparisonProgressBar.cs
+++ KysectAcademyTask/Utils/ProgressTracking/ConsoleComparisonProgressBar.cs
@@ -5,20 +5,21 @@ internal class ConsoleComparisonProgressBar : IProgressBar
     public void Update(int workToDo, int workDone, int percentage)
     {
         Console.Clear();
-        Console.Write($"\t\t\t{percentage,3}% done. Compared {workDone} / {workToDo} submits\n");
+        Console.Write($"\t\t\t{percentage,3}% done. Compared {workDone} / {workToDo} pairs of submits\n");
         Draw(percentage);
     }
 
     private void Draw(int percentage)
     {
-        Console.CursorLeft = 0;
-        Console.Write("[");
-        Console.CursorLeft = 101;
-        Console.Write("]");
-        Console.CursorLeft = 1;
+        var pbSetup = new ConsoleProgressBarSetup('[', ']', '#', 0, 101, 1);
+        Console.CursorLeft = pbSetup.LeftCursorPos;
+        Console.Write(pbSetup.LeftBorderSymbol);
+        Console.CursorLeft = pbSetup.RightCursorPos;
+        Console.Write(pbSetup.RightBorderSymbol);
+        Console.CursorLeft = pbSetup.BeginningCursorPos;
         for (int i = 0; i < percentage; ++i)
         {
-            Console.Write("#");
+            Console.Write(pbSetup.ProgressSymbol);
         }
     }
 }
