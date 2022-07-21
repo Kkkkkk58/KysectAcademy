28f7c58a48287a6335ea4abc16703117c6a8224c
Karim Khasan <karimhasan3879@gmail.com>
2022-07-21 13:23:06 +0300

    refactor: reorder ctor and prop

--- KysectAcademyTask/Utils/DirectoryPathSplitter.cs
+++ KysectAcademyTask/Utils/DirectoryPathSplitter.cs
@@ -2,15 +2,15 @@
 
 internal class DirectoryPathSplitter
 {
-    public IReadOnlyList<string> SplitDirectories
-    {
-        get => _splitDirectories;
-    }
-
     private readonly string[] _splitDirectories;
 
     public DirectoryPathSplitter(string path)
     {
         _splitDirectories = path.Split(Path.DirectorySeparatorChar, StringSplitOptions.RemoveEmptyEntries);
+    }
+
+    public IReadOnlyList<string> SplitDirectories
+    {
+        get => _splitDirectories;
     }
 }
