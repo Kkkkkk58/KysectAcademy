78b8446bdcc0fbb07bc9d17636324c101179a27b
Karim Khasan <karimhasan3879@gmail.com>
2022-07-30 17:53:42 +0300

    refactor: extract methods

--- KysectAcademyTask/DbInteraction/DbPreparer.cs
+++ KysectAcademyTask/DbInteraction/DbPreparer.cs
@@ -24,60 +24,90 @@ internal class DbPreparer
     public void Prepare(IReadOnlyCollection<SubmitInfo> submits)
     {
         var submitsToAdd = new List<DataAccess.Models.Entities.Submit>();
+
         foreach (SubmitInfo submit in submits)
         {
 
-            if (!_groupRepo.GetQueryWithProps(submit.GroupName).Any())
-            {
-                _groupRepo.Add(new Group
-                {
-                    Name = submit.GroupName
-                });
-            }
-
-            if (!_homeWorkRepo.GetQueryWithProps(submit.HomeworkName).Any())
-            {
-                _homeWorkRepo.Add(new HomeWork
-                {
-                    Name = submit.HomeworkName
-                });
-            }
-
-            string[] splitName = submit.AuthorName.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-            if (!_studentRepo.GetQueryWithProps(splitName[0], splitName[1], submit.GroupName).Any())
-            {
-                IQueryable<Group> groupQuery = _groupRepo.GetQueryWithProps(submit.GroupName);
-                int groupId = groupQuery.Single().Id;
-
-                _studentRepo.Add(new Student
-                {
-                    GroupId = groupId,
-                    PersonalInformation = new Person
-                    {
-                        FirstName = splitName[0],
-                        LastName = splitName[1]
-                    }
-                });
-            }
-
-            if (!_submitRepo.GetQueryWithProps(submit.AuthorName, submit.GroupName, submit.HomeworkName, submit.SubmitDate).Any())
-            {
-                IQueryable<HomeWork> homeWorkQuery = _homeWorkRepo.GetQueryWithProps(submit.HomeworkName);
-                int homeWorkId = homeWorkQuery.Single().Id;
-
-                IQueryable<Student> studentQuery =
-                    _studentRepo.GetQueryWithProps(splitName[0], splitName[1], submit.GroupName);
-                int studentId = studentQuery.Single().Id;
-
-                submitsToAdd.Add(new DataAccess.Models.Entities.Submit
-                {
-                    Date = submit.SubmitDate,
-                    HomeWorkId = homeWorkId,
-                    StudentId = studentId
-                });
-            }
+            PrepareGroup(submit.GroupName);
+            PrepareHomeWork(submit.HomeworkName);
+            PrepareAuthor(submit.AuthorName, submit.GroupName);
+            PrepareSubmitAddToList(submit, submitsToAdd);
         }
 
         _submitRepo.AddRange(submitsToAdd);
+    }
+    
+    private void PrepareGroup(string groupName)
+    {
+        if (!_groupRepo.GetQueryWithProps(groupName).Any())
+        {
+            _groupRepo.Add(new Group
+            {
+                Name = groupName
+            });
+        }
+    }
+
+    private void PrepareHomeWork(string homeWorkName)
+    {
+        if (!_homeWorkRepo.GetQueryWithProps(homeWorkName).Any())
+        {
+            _homeWorkRepo.Add(new HomeWork
+            {
+                Name = homeWorkName
+            });
+        }
+    }
+
+    private void PrepareAuthor(string authorName, string groupName)
+    {
+        string[] splitName = authorName.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        if (splitName.Length != 2)
+        {
+            throw new ArgumentException($"Unable to work with author's name: {authorName}");
+        }
+
+        if (_studentRepo.GetQueryWithProps(splitName[0], splitName[1], groupName).Any())
+        {
+            return;
+        }
+
+        IQueryable<Group> groupQuery = _groupRepo.GetQueryWithProps(groupName);
+        int groupId = groupQuery.Single().Id;
+
+        _studentRepo.Add(new Student
+        {
+            GroupId = groupId,
+            PersonalInformation = new Person
+            {
+                FirstName = splitName[0],
+                LastName = splitName[1]
+            }
+        });
+    }
+
+    private void PrepareSubmitAddToList(SubmitInfo submit, ICollection<DataAccess.Models.Entities.Submit> submitsToAdd)
+    {
+        if (_submitRepo
+            .GetQueryWithProps(submit.AuthorName, submit.GroupName, submit.HomeworkName, submit.SubmitDate)
+            .Any())
+        {
+            return;
+        }
+
+        IQueryable<HomeWork> homeWorkQuery = _homeWorkRepo.GetQueryWithProps(submit.HomeworkName);
+        int homeWorkId = homeWorkQuery.Single().Id;
+
+        string[] splitName = submit.AuthorName.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        IQueryable<Student> studentQuery =
+            _studentRepo.GetQueryWithProps(splitName[0], splitName[1], submit.GroupName);
+        int studentId = studentQuery.Single().Id;
+
+        submitsToAdd.Add(new DataAccess.Models.Entities.Submit
+        {
+            Date = submit.SubmitDate,
+            HomeWorkId = homeWorkId,
+            StudentId = studentId
+        });
     }
 }
