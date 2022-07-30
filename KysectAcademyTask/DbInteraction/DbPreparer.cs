7fc242db436b1cabb3dab81768301f6c60f4f6fa
Karim Khasan <karimhasan3879@gmail.com>
2022-07-30 17:37:43 +0300

    refactor: clarify logic of adding local entites

--- KysectAcademyTask/DbInteraction/DbPreparer.cs
+++ KysectAcademyTask/DbInteraction/DbPreparer.cs
@@ -23,45 +23,33 @@ internal class DbPreparer
 
     public void Prepare(IReadOnlyCollection<SubmitInfo> submits)
     {
-        var groupsToAdd = new List<Group>();
-        var homeWorksToAdd = new List<HomeWork>();
-        var studentsToAdd = new List<Student>();
         var submitsToAdd = new List<DataAccess.Models.Entities.Submit>();
-
         foreach (SubmitInfo submit in submits)
         {
-            if (!_groupRepo.GetQueryWithProps(submit.GroupName).Any() && groupsToAdd.All(g => g.Name != submit.GroupName))
+
+            if (!_groupRepo.GetQueryWithProps(submit.GroupName).Any())
             {
-                groupsToAdd.Add(new Group
+                _groupRepo.Add(new Group
                 {
                     Name = submit.GroupName
                 });
             }
 
-            if (!_homeWorkRepo.GetQueryWithProps(submit.HomeworkName).Any() &&
-                homeWorksToAdd.All(h => h.Name != submit.HomeworkName))
+            if (!_homeWorkRepo.GetQueryWithProps(submit.HomeworkName).Any())
             {
-                homeWorksToAdd.Add(new HomeWork
+                _homeWorkRepo.Add(new HomeWork
                 {
                     Name = submit.HomeworkName
                 });
             }
 
             string[] splitName = submit.AuthorName.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-            if (!_studentRepo.GetQueryWithProps(splitName[0], splitName[1], submit.GroupName).Any()
-                && studentsToAdd.All(s => s.PersonalInformation.FirstName != splitName[0] && s.PersonalInformation.LastName != splitName[1]
-                    && _groupRepo.FindAsNoTracking(s.GroupId).Name != submit.GroupName))
+            if (!_studentRepo.GetQueryWithProps(splitName[0], splitName[1], submit.GroupName).Any())
             {
                 IQueryable<Group> groupQuery = _groupRepo.GetQueryWithProps(submit.GroupName);
-                if (!groupQuery.Any())
-                {
-                    _groupRepo.AddRange(groupsToAdd);
-                    groupsToAdd.Clear();
-                    
-                }
                 int groupId = groupQuery.Single().Id;
 
-                studentsToAdd.Add(new Student
+                _studentRepo.Add(new Student
                 {
                     GroupId = groupId,
                     PersonalInformation = new Person
@@ -75,20 +63,10 @@ internal class DbPreparer
             if (!_submitRepo.GetQueryWithProps(submit.AuthorName, submit.GroupName, submit.HomeworkName, submit.SubmitDate).Any())
             {
                 IQueryable<HomeWork> homeWorkQuery = _homeWorkRepo.GetQueryWithProps(submit.HomeworkName);
-                if (!homeWorkQuery.Any())
-                {
-                    _homeWorkRepo.AddRange(homeWorksToAdd);
-                    homeWorksToAdd.Clear();
-                }
                 int homeWorkId = homeWorkQuery.Single().Id;
 
                 IQueryable<Student> studentQuery =
                     _studentRepo.GetQueryWithProps(splitName[0], splitName[1], submit.GroupName);
-                if (!studentQuery.Any())
-                {
-                    _studentRepo.AddRange(studentsToAdd);
-                    studentsToAdd.Clear();
-                }
                 int studentId = studentQuery.Single().Id;
 
                 submitsToAdd.Add(new DataAccess.Models.Entities.Submit
@@ -100,9 +78,6 @@ internal class DbPreparer
             }
         }
 
-        _groupRepo.AddRange(groupsToAdd);
-        _studentRepo.AddRange(studentsToAdd);
-        _homeWorkRepo.AddRange(homeWorksToAdd);
         _submitRepo.AddRange(submitsToAdd);
     }
 }
